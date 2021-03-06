f9faaae31ac98141651423ba8400ffcb342ef1ca
xamqrexii <usama1630@gmail.com>
2021-03-06 09:46:39 +0500

    feat: setup apollo-server-express

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import express from "express";
 import { success } from "consola";
+import { ApolloServer, gql } from "apollo-server-express";
 import { PORT } from "./config";
 
-// initialize app
+// initialize express app
 const app = express();
 
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+// initialize apollo-express server
+const server = new ApolloServer({ typeDefs, resolvers, context: {} });
+
 const startApp = () => {
+  // inject apollo-express middleware
+  server.applyMiddleware({ app });
+
+  // start express server
   app.listen(PORT, () =>
     success({
-      message: `🚀 Server ready at http://localhost:${PORT}`,
+      message: `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`,
       badge: true,
     })
   );
